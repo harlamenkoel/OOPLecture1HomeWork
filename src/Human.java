e939e1fdfcd3b1fdfdd957306cdf27a41eeef180
harlamenkoel <harlamenkoel@gmail.com>
2022-12-06 00:56:47 +0700

    Конец

--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,43 @@
+import java.util.Calendar;
+
 public class Human {
+    String name;
+    int yearOfBirth;
+    String town;
+    String job;
+
+    Human(String name, int age, String town, String job) {
+        Calendar calendar = Calendar.getInstance();
+        int currentYear = calendar.get(Calendar.YEAR);
+
+        if (age < 0) {
+            yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = currentYear - age;
+        }
+        if (name.equals("")) {
+            name = "Информация не указана";
+        }
+        if (town.equals("")) {
+            town = "Информация не указана";
+        }
+        if (job.equals("")) {
+            job = "Информация не указана";
+        }
+
+        this.name = name;
+        this.town = town;
+        this.job = job;
+    }
+
+    void welcomeMessage() {
+        System.out.println("Привет! Меня зовут " + name + ". " +
+                "Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!");
+    }
+
+    void welcomeMessageWomen() {
+        System.out.println("Привет! Меня зовут " + name + ". " +
+                "Я из города " + town + ". Я родилась в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!");
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Human maxim = new Human("Максим", -5, "Минск", "бренд-менеджер");
+        Human ann = new Human("", 29, "Москва", "методист образовательных программ");
+        Human kate = new Human("Екатерина", 28, "Калининград", "");
+        Human artem = new Human("Артем", 27, "", "директор по развитию бизнеса");
 
-        System.out.println("Hello world!");
+        maxim.welcomeMessage();
+        ann.welcomeMessageWomen();
+        kate.welcomeMessageWomen();
+        artem.welcomeMessage();
+
     }
 }
